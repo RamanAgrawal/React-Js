65dd56f790e1524aefcc47d63417ed742df15502
Explorewithcoder <khussharma599@gmail.com>
2023-03-02 13:01:15 +0530

    task-2-Advance-react

--- task-10/src/App.js
+++ task-10/src/App.js
@@ -4,14 +4,20 @@ import ExpenseItems from "./Components/ExpenseItems";
 function App() {
   const expenses = [
     { title: "Food", amount: "200 Rs", date: new Date(2023, 3, 1) },
+    { title: "bakery", amount: "200 Rs", date: new Date(2023, 3, 1) },
+    { title: "Books", amount: "200 Rs", date: new Date(2023, 3, 1) },
     { title: "Cloths", amount: "300 Rs", date: new Date(2023, 3, 1) },
-    { title: "Cloths", amount: "400 Rs", date: new Date(2023, 3, 1) },
-    { title: "Cloths", amount: "500 Rs", date: new Date(2023, 3, 1) },
-    { title: "Cloths", amount: "600 Rs", date: new Date(2023, 3, 1) },
-    { title: "Cloths", amount: "700 Rs", date: new Date(2023, 3, 1) },
-    { title: "Cloths", amount: "800 Rs", date: new Date(2023, 3, 1) },
-    { title: "Cloths", amount: "900 Rs", date: new Date(2023, 3, 1) },
-    { title: "Cloths", amount: "1000 Rs", date: new Date(2023, 3, 1) },
+    { title: "Grocery", amount: "400 Rs", date: new Date(2023, 3, 1) },
+    { title: "Shops", amount: "500 Rs", date: new Date(2023, 3, 1) },
+    { title: "Bath", amount: "200 Rs", date: new Date(2023, 3, 1) },
+    { title: "Petrol", amount: "300 Rs", date: new Date(2023, 3, 1) },
+    { title: "Desiel", amount: "400 Rs", date: new Date(2023, 3, 1) },
+    { title: "Vegetables", amount: "500 Rs", date: new Date(2023, 3, 1) },
+    { title: "Egg", amount: "200 Rs", date: new Date(2023, 3, 1) },
+    { title: "Paneer", amount: "300 Rs", date: new Date(2023, 3, 1) },
+    { title: "Suit", amount: "400 Rs", date: new Date(2023, 3, 1) },
+    { title: "Pant", amount: "500 Rs", date: new Date(2023, 3, 1) },
+    { title: "Shirt", amount: "200 Rs", date: new Date(2023, 3, 1) },
   ];
 
   return (
--- task-10/src/Components/ExpenseItems.jsx
+++ task-10/src/Components/ExpenseItems.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import ExpenseAmount from "./ExpenseAmount";
 import ExpenseDate from "./ExpenseDate";
+// import { useRef } from "react";
 const ExpenseItems = (props) => {
+  // const Delete = useRef();
+  // const handleClick = (e) => {
+  //   Delete.current(e.target.parentNode.remove());
+  // };
+
+  const handleClick = (e) => {
+    e.target.parentNode.remove();
+  };
+
   return (
     <div>
+      {/* <table ref={Delete} border={"1"} cellSpacing={"0"}> */}
       <table border={"1"} cellSpacing={"0"}>
         <thead>
           <tr>
             <th>Date</th>
+            <th>Title</th>
             <th>Amount</th>
           </tr>
         </thead>
@@ -16,13 +28,27 @@ const ExpenseItems = (props) => {
             <td>
               <ExpenseDate date={props.exDate} />
             </td>
-
+            <td>{props.title}</td>
             <td>
               <ExpenseAmount amount={props.amount} />
             </td>
           </tr>
         </tbody>
       </table>
+      <button
+        onClick={handleClick}
+        style={{
+          display: "block",
+          margin: "auto",
+          padding: "4px",
+          fontSize: "16px",
+          background: "red",
+          border: "none",
+          cursor: "pointer",
+        }}
+      >
+        Delete Expense
+      </button>
     </div>
   );
 };
