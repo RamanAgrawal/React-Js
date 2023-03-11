d05579b0f668105f5d8946fbe438d2ae9692014e
Explorewithcoder <khussharma599@gmail.com>
2023-03-11 20:31:17 +0530

    task-7-again

--- Final2/Ecommerce/task-6/src/App.js
+++ Final2/Ecommerce/task-6/src/App.js
@@ -12,29 +12,36 @@ function App() {
       let res = await axios.get("https://swapi.dev/api/films");
       console.log(res.data.results);
       setData(res.data.results);
-      setLoading(true);
+      setLoading(false);
     } catch (error) {
       setLoading(false);
       console.log("error:", error);
     }
   };
 
+  if (loading) {
+    return (
+      <img
+        style={{ display: "block", margin: "auto" }}
+        src="https://media4.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif"
+        alt="image"
+      />
+    );
+  }
+
   return (
     <div className="App">
       <button onClick={fetch}>Fetch Movies</button>
-      {loading && (
-        <div>
-          {data.map((el) => {
-            return (
-              <div key={el.title}>
-                {el.title}
-                <p>{el.opening_crawl}</p>
-              </div>
-            );
-          })}
-        </div>
-      )}
-      {loading && <p>loading</p>}
+      <div>
+        {data.map((el) => {
+          return (
+            <div key={el.title}>
+              {el.title}
+              <p>{el.opening_crawl}</p>
+            </div>
+          );
+        })}
+      </div>
       {!loading && data.length === 0 && <p>No movies</p>}
     </div>
   );
