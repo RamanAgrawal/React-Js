91ef137e9fcf69da5d2ce3ce03d25b8d89173863
Explorewithcoder <khussharma599@gmail.com>
2023-03-11 20:25:06 +0530

    task-7

--- Final2/Ecommerce/task-6/src/App.js
+++ Final2/Ecommerce/task-6/src/App.js
@@ -4,13 +4,17 @@ import "./App.css";
 
 function App() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const fetch = async () => {
     try {
+      setLoading(true);
       let res = await axios.get("https://swapi.dev/api/films");
       console.log(res.data.results);
       setData(res.data.results);
+      setLoading(true);
     } catch (error) {
+      setLoading(false);
       console.log("error:", error);
     }
   };
@@ -18,14 +22,20 @@ function App() {
   return (
     <div className="App">
       <button onClick={fetch}>Fetch Movies</button>
-      {data.map((el) => {
-        return (
-          <div key={el.title}>
-            {el.title}
-            <p>{el.opening_crawl}</p>
-          </div>
-        );
-      })}
+      {loading && (
+        <div>
+          {data.map((el) => {
+            return (
+              <div key={el.title}>
+                {el.title}
+                <p>{el.opening_crawl}</p>
+              </div>
+            );
+          })}
+        </div>
+      )}
+      {loading && <p>loading</p>}
+      {!loading && data.length === 0 && <p>No movies</p>}
     </div>
   );
 }
