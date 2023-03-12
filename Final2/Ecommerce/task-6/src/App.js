ee1beef47bf1c3ad619f86313640f264b7d4c327
Explorewithcoder <khussharma599@gmail.com>
2023-03-12 11:07:51 +0530

    task-10

--- Final2/Ecommerce/task-6/src/App.js
+++ Final2/Ecommerce/task-6/src/App.js
@@ -6,6 +6,21 @@ function App() {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [title, setTitle] = useState("");
+  const [area, setArea] = useState("");
+  const [date, setDate] = useState("");
+
+  const handleTitle = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleArea = (e) => {
+    setArea(e.target.value);
+  };
+
+  const handleDate = (e) => {
+    setDate(e.target.value);
+  };
 
   const handleError = () => {
     setError(false);
@@ -24,6 +39,18 @@ function App() {
       throw new Error("Something went wrong ....Retrying");
     }
   });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const details = {
+      title,
+      area,
+      date,
+    };
+    console.log(details);
+    console.log("heo");
+  };
+
   useEffect(() => {
     fetch();
   }, []);
@@ -40,6 +67,32 @@ function App() {
 
   return (
     <div className="App">
+      <form>
+        <div>
+          <label>Title</label>
+          <input
+            onChange={handleTitle}
+            type={"text"}
+            placeholder={"Enter Title Here"}
+          />
+        </div>
+        <div>
+          <label>Opening Text</label>
+          <textarea
+            placeholder={"Enter Title Here"}
+            onChange={handleArea}
+          ></textarea>
+        </div>
+        <div>
+          <label>Release Date</label>
+          <input
+            onChange={handleDate}
+            type={"text"}
+            placeholder={"Enter Release Date"}
+          />
+        </div>
+        <input onClick={handleSubmit} type={"submit"} value={"ADD MOVIE"} />
+      </form>
       <button onClick={fetch}>Fetch</button>
       <button onClick={handleError}>Cancle</button>
       <div>
