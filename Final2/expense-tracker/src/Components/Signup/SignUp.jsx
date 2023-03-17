a15fdb190f8c572f98a945cb57fd7f002152d0f7
Explorewithcoder <khussharma599@gmail.com>
2023-03-17 09:29:19 +0530

    1

---
+++ Final2/expense-tracker/src/Components/Signup/SignUp.jsx
@@ -0,0 +1,62 @@
+import axios from "axios";
+import React, { useState } from "react";
+
+const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [pass, setPass] = useState("");
+  const [conPass, setConPass] = useState("");
+
+  const handleEMail = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePassword = (e) => {
+    setPass(e.target.value);
+  };
+
+  const handleConPass = (e) => {
+    setConPass(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (email === "" || pass === "" || conPass === "") {
+      alert("Please Fill All Fields");
+    } else if (conPass != pass) {
+      alert("Password Not Matched");
+    } else {
+      try {
+        let res = await axios.post(
+          "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCFrmedDfSLLubh6dopFm8w_kt-t0eGWRA",
+          {
+            email: email,
+            password: pass,
+            returnSecureToken: true,
+          }
+        );
+        console.log(res);
+      } catch (error) {
+        console.log("error:", error);
+      }
+    }
+  };
+
+  return (
+    <form>
+      <input onChange={handleEMail} type="text" placeholder="Enter Email" />
+      <input
+        onChange={handlePassword}
+        type="password"
+        placeholder="Enter Password"
+      />
+      <input
+        onChange={handleConPass}
+        type="text"
+        placeholder="Confirm Password"
+      />
+      <input onClick={handleSubmit} type="submit" value={"Submit"} />
+    </form>
+  );
+};
+
+export default SignUp;
