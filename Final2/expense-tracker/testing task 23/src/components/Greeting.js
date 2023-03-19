70b6424ec02909c468e17e944e20faf79b769876
Explorewithcoder <khussharma599@gmail.com>
2023-03-19 15:02:58 +0530

    24-test-done

--- Final2/expense-tracker/testing task 23/src/components/Greeting.js
+++ Final2/expense-tracker/testing task 23/src/components/Greeting.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+import Output from "./Output";
 
 const Greeting = () => {
+  const [text, changeText] = useState(false);
+
+  const handleCLick = () => {
+    changeText(true);
+  };
+
   return (
     <div>
       <h2>Hello Khushhal</h2>
-      <p>It's good to see you !</p>
+      {!text && <Output>It's good to see you !</Output>}
+      {text && <Output>Changed !</Output>}
+      <button onClick={handleCLick}>Change Text</button>
     </div>
   );
 };
--- Final2/expense-tracker/testing task 23/src/components/Greeting.test.js
+++ Final2/expense-tracker/testing task 23/src/components/Greeting.test.js
@@ -1,9 +1,48 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Greeting from "./Greeting";
 describe("Greeting Component", () => {
   test("Hello Khushhal", () => {
     render(<Greeting />);
     const getText = screen.getByText("Hello Khushhal");
     expect(getText).toBeInTheDocument();
+  });
+
+  test("renders good to see you if the button was not clicked", () => {
+    render(<Greeting />);
+    const para = screen.getByText("good to see you", { exact: false });
+    expect(para).toBeInTheDocument();
+  });
+
+  test("render changed if the button was clickd", () => {
+    // arrange
+
+    render(<Greeting />);
+
+    // act
+
+    const button = screen.getByRole("button");
+    userEvent.click(button);
+
+    // assert
+
+    const para = screen.getByText("Changed !");
+    expect(para).toBeInTheDocument();
+  });
+
+  test("does not render good to see you", () => {
+    // arrange
+
+    render(<Greeting />);
+
+    // act
+
+    const button = screen.getByRole("button");
+    userEvent.click(button);
+
+    // assert
+
+    const para = screen.queryByText("good to see you", { exact: false });
+    expect(para).toBeNull();
   });
 });
